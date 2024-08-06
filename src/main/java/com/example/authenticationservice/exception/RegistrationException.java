87e79d276ca80dee41b63811f827df647a4d334e
TemaGreen1997 <arhamidullin@gmail.com>
2024-08-06 23:28:12 +0400

    add RegistrationException and LoginException

---
+++ src/main/java/com/example/authenticationservice/exception/RegistrationException.java
@@ -0,0 +1,8 @@
+package com.example.authenticationservice.exception;
+
+public class RegistrationException extends RuntimeException {
+
+    public RegistrationException(String message) {
+        super(message);
+    }
+}
